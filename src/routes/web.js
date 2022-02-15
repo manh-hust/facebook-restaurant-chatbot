b4bc11b52742cd8439ca0d28bf7f41bf19cafcd8
ongimkshp <doducmanh.bb@gmail.com>
2022-02-15 21:51:12 +0700

    second-commit

---
+++ src/routes/web.js
@@ -0,0 +1,15 @@
+import express from "express";
+import homeController from "../controller/HomeController"
+
+let router = express.Router()
+
+
+let initWebRoutes = app => {
+    router.get("/",homeController.getHomePage)
+    router.post('/webhook', homeController.postWebhook);
+    router.get('/webhook', homeController.getWebhook)
+
+    return app.use('/', router)
+}
+
+module.exports = initWebRoutes
