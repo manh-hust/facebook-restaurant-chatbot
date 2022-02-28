3a256cb3deccfa9576372b742909f794451067cf
ongimkshp <doducmanh.bb@gmail.com>
2022-02-28 12:46:20 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import webRoutes from './routes/web'
 
 let app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 viewEngine(app);
 
 webRoutes(app);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
 
 let port = process.env.PORT || 8080;
 
