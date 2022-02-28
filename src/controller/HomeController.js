fd3f1a27e5d2417c0d66de42120bb112678e3e72
ongimkshp <doducmanh.bb@gmail.com>
2022-02-28 17:02:01 +0700

    add-get-started-button

--- src/controller/HomeController.js
+++ src/controller/HomeController.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 import request from 'request'
 
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
+
 let getHomePage = (req, res) => {
   return res.render('homepage.ejs')
 }
@@ -159,8 +161,33 @@ function callSendAPI(sender_psid, response) {
   });
 }
 
+let setupProfile = (req, res) => {
+  // Construct the message body
+  let request_body = {
+    "get_started": "GET_STARTED",
+    "whitelisted_domains": "https://restaurant-chatbot-app-manh.herokuapp.com/"
+  }
+
+  // Send the HTTP request to the Messenger Platform
+  request({
+    "uri": `https://graph.facebook.com/v13.0/me/messenger_profile?access_token=${PAGE_ACCESS_TOKEN}`,
+    "qs": {
+      "access_token": PAGE_ACCESS_TOKEN
+    },
+    "method": "POST",
+    "json": request_body
+  }, (err, res, body) => {
+    if (!err) {
+      console.log('Setup profile success!')
+    } else {
+      console.error("Unable to setup profile:" + err);
+    }
+  });
+}
+
 module.exports = {
   getHomePage: getHomePage,
   postWebhook: postWebhook,
-  getWebhook: getWebhook
+  getWebhook: getWebhook,
+  setupProfile: setupProfile
 }
--- src/routes/web.js
+++ src/routes/web.js
@@ -5,7 +5,8 @@ let router = express.Router()
 
 
 let initWebRoutes = app => {
-    router.get("/",homeController.getHomePage)
+    router.get("/", homeController.getHomePage)
+    router.post("/setup-profile", homeController.setupProfile)
     router.post('/webhook', homeController.postWebhook);
     router.get('/webhook', homeController.getWebhook)
 
