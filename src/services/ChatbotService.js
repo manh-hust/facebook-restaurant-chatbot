2cbd66a3f304ab1987fb7a6b26c043d958010331
ongimkshp <doducmanh.bb@gmail.com>
2022-03-04 15:53:38 +0700

    update

--- src/services/ChatbotService.js
+++ src/services/ChatbotService.js
@@ -30,11 +30,34 @@ let callSendAPI = (sender_psid, response) => {
     });
 }
 
+let getUserName = async (sender_psid) => {
+    // Send the HTTP request to the Messenger Platform
+    let userName = ''
+    await request({
+        "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+        "qs": {
+            "access_token": PAGE_ACCESS_TOKEN
+        },
+        "method": "GET",
+    }, (err, res, body) => {
+        console.log('Body la: ' + body)
+        if (!err) {
+            let response = JSON.parse(res);
+            userName = `${response.first_name} ${response.last_name}`
+            console.log('message sent!')
+        } else {
+            console.error("Unable to send message:" + err);
+        }
+    });
+    return userName
+}
+
 let handleGetStarted = (sender_psid) => {
     return new Promise(async (resolve, reject) => {
         try {
+            let userName = await getUserName(sender_psid)
             let response = {
-                "text": "OK! Chào mừng bạn đến với nhà hàng của chúng tôi."
+                "text": `OK! Chào mừng ${userName} đến với nhà hàng của chúng tôi.`
             }
             await callSendAPI(sender_psid, response)
             resolve('done')
