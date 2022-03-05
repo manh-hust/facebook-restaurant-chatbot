bbf180b830e79c5f5b7b0a8eaffc9767781ba9c7
ongimkshp <doducmanh.bb@gmail.com>
2022-03-05 16:23:37 +0700

    update-05

--- src/services/ChatbotService.js
+++ src/services/ChatbotService.js
@@ -3,7 +3,11 @@ import request from 'request'
 require('dotenv').config();
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
-const linkImage = 'https://www.schiavello.com/__data/assets/image/0014/8105/carousel-atlantic-restaurant-crown-dining-partition.jpg'
+const IMAGE_GET_STARTED = 'https://www.schiavello.com/__data/assets/image/0014/8105/carousel-atlantic-restaurant-crown-dining-partition.jpg'
+const IMAGE_MAIN_MENU_1 = 'https://media-cdn.tripadvisor.com/media/photo-s/17/75/3f/d1/restaurant-in-valkenswaard.jpghttps://media.gettyimages.com/photos/pan-fried-duck-picture-id1081422898?s=612x612'
+const IMAGE_MAIN_MENU_2 = 'https://gosvietnam.vn/wp-content/themes/gosvn/images/thietkephanmem/img-video-4.jpg'
+const IMAGE_MAIN_MENU_3 = 'https://media-cdn.tripadvisor.com/media/photo-s/17/75/3f/d1/restaurant-in-valkenswaard.jpg'
+
 
 let callSendAPI = (sender_psid, response) => {
     // Construct the message body
@@ -60,9 +64,13 @@ let handleGetStarted = (sender_psid) => {
             let response1 = {
                 "text": `Chào mừng ${userName} đến với nhà hàng của chúng tôi.`
             }
-            let response2 = sendGetStartedTemplate()
+            let response2 = getStartedTemplate()
+
+            // Send text message
             callSendAPI(sender_psid, response1)
+            // Send generic template message
             callSendAPI(sender_psid, response2)
+
             resolve('done')
         } catch (e) {
             reject(e)
@@ -70,14 +78,78 @@ let handleGetStarted = (sender_psid) => {
     })
 }
 
-let sendGetStartedTemplate = () => {
+let handleMainMenu = (sender_psid) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+
+            let response2 = getMainMenuTemplate()
+            // Send generic template message
+            callSendAPI(sender_psid, response2)
+
+            resolve('done')
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+let getMainMenuTemplate = () => {
     let response = {
         "attachment": {
             "type": "template",
             "payload": {
                 "template_type": "generic",
                 "elements": [{
-                    "title": "Nhà nhà Pikalazada kính chào quý khách.",
+                        "title": "Menu của nhà hàng",
+                        "subtitle": "Chúng tôi hân hạnh mang đến cho bạn thực đơn phong phú của bữa trưa và bữa tối.",
+                        "image_url": IMAGE_MAIN_MENU_1,
+                        "buttons": [{
+                                "type": "postback",
+                                "title": "Bữa trưa",
+                                "payload": "LUNCH_MENU",
+                            },
+                            {
+                                "type": "postback",
+                                "title": "Bữa tối",
+                                "payload": "DINNER_MENU",
+                            }
+                        ],
+                    },
+                    {
+                        "title": "Giờ mở cửa",
+                        "subtitle": "T2 -> T6 : 10AM - 10PM || T7 + CN : 8AM - 11PM",
+                        "image_url": IMAGE_MAIN_MENU_2,
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "ĐẶT BÀN",
+                            "payload": "RESERVE_TABLE",
+                        }, ],
+                    },
+                    {
+                        "title": "Không gian nhà hàng",
+                        "subtitle": "Nhà hàng có sức chứa lên đến 300 khách và phục vụ các bữa tiệc lớn.",
+                        "image_url": IMAGE_MAIN_MENU_3,
+                        "buttons": [{
+                            "type": "postback",
+                            "title": "CHI TIẾT",
+                            "payload": "SHOW_ROOMS",
+                        }, ],
+                    }
+                ]
+            }
+        }
+    }
+    return response
+}
+
+let getStartedTemplate = () => {
+    let response = {
+        "attachment": {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{
+                    "title": "Nhà hàng Pikalazada kính chào quý khách.",
                     "subtitle": "Dưới đây là các dịch vụ của nhà hàng.",
                     "image_url": linkImage,
                     "buttons": [{
@@ -104,5 +176,6 @@ let sendGetStartedTemplate = () => {
 }
 
 module.exports = {
-    handleGetStarted: handleGetStarted
+    handleGetStarted: handleGetStarted,
+    handleMainMenu: handleMainMenu
 }
